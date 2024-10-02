7eadf786a0c2de9c3bb2e7fc0b97f0e5012e558d
durongze <durongze@qq.com>
2024-10-02 22:23:33 +0800

    SET

--- src/cmake_file.cpp
+++ src/cmake_file.cpp
@@ -1,10 +1,13 @@
 #include "cmake_file.h"
 
-CMakeFile::CMakeFile(const char* filename):m_name(filename) {
+#include <algorithm>
+#include <cctype>
+
+CMakeFile::CMakeFile(const char* filename):m_name(filename), m_ProjName("CMAKE_PROJ_NAME") {
 
 }
 
-CMakeFile::CMakeFile(const std::string& filename) :m_name(filename) {
+CMakeFile::CMakeFile(const std::string& filename) :m_name(filename), m_ProjName("CMAKE_PROJ_NAME") {
 
 }
 
@@ -27,13 +30,45 @@ int CMakeFile::write(const std::string& txt)
 	return 0;
 }
 
-int CMakeFile::write(const std::map<std::string, int>& cppMap)
+std::string ToUpperStr(std::string str)
+{
+	std::transform(str.begin(), str.end(), str.begin(),
+		[](unsigned char c) { return std::toupper(c); });
+	return str;
+}
+
+int CMakeFile::write(const std::map<std::string, int>& fileMap, int FileType)
 {
 	std::map<std::string, int>::const_iterator iterCppMap;
-	m_fs.open(m_name, std::ios::out | std::ios::app);
-	for (iterCppMap = cppMap.cbegin(); iterCppMap != cppMap.cend(); iterCppMap++) {
-		m_fs << (iterCppMap->first) << std::endl;
+
+	if (fileMap.size() == 0) {
+		return 0;
 	}
+
+	m_fs.open(m_name, std::ios::out | std::ios::app);
+	m_fs << "set (";
+	switch (FileType) {
+		case FILE_TYPE_ASM:
+			m_fs << ToUpperStr(m_ProjName) << "_ASM    ";
+			break;
+		case FILE_TYPE_CPP:
+			m_fs << ToUpperStr(m_ProjName) << "_CPP    ";
+			break;
+		case FILE_TYPE_HPP:
+			m_fs << ToUpperStr(m_ProjName) << "_HPP    ";
+			break;
+		case FILE_TYPE_RES:
+			m_fs << ToUpperStr(m_ProjName) << "_RES    ";
+			break;
+		default:
+			m_fs << ToUpperStr(m_ProjName) << "_DEF    ";
+			break;
+	}
+	m_fs << std::endl;
+	for (iterCppMap = fileMap.cbegin(); iterCppMap != fileMap.cend(); iterCppMap++) {
+		m_fs << "    " << (iterCppMap->first) << std::endl;
+	}
+	m_fs << ")" << std::endl;
 	m_fs.close();
 	return 0;
 }
--- src/cmake_file.h
+++ src/cmake_file.h
@@ -7,17 +7,28 @@
 #include <strstream>
 #include <map>
 
+#define CMAKE_FIILE_TO_STR(X)   #X
+
 class CMakeFile{
+public:
+	enum FileType {
+		FILE_TYPE_DEF,
+		FILE_TYPE_ASM,
+		FILE_TYPE_CPP,
+		FILE_TYPE_HPP,
+		FILE_TYPE_RES
+	};
 public:
 	CMakeFile(const char* filename);
 	CMakeFile(const std::string& filename);
 	~CMakeFile();
 	int create();
 	int write(const std::string& txt);
-	int write(const std::map<std::string, int>& cppMap);
+	int write(const std::map<std::string, int>& fileMap,  int fileType = 0);
 private:
 	std::fstream m_fs;
     std::string  m_name;
+	std::string m_ProjName;
 };
 
 #endif
